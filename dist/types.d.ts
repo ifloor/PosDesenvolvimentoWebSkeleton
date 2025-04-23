c53e0ff25d3d53563d9eafd7d4901cbdb2b28e4d
Igor Floor <igor@mikha-el.com>
2025-04-23 02:56:59 -0300

    Commiting .d.ts types

---
+++ dist/types.d.ts
@@ -0,0 +1,15 @@
+export declare enum ProductCategory {
+    Moda = 0,
+    Eletronicos = 1,
+    Alimentos = 2,
+    Bebidas = 3,
+    CasaDecoracao = 4,
+    SaudeBeleza = 5,
+    EsporteLazer = 6,
+    Livros = 7,
+    Papelaria = 8,
+    Brinquedos = 9,
+    Jogos = 10,
+    Petshop = 11,
+    Automotivos = 12
+}
---
+++ dist/types.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ProductCategory = void 0;
+var ProductCategory;
+(function (ProductCategory) {
+    ProductCategory[ProductCategory["Moda"] = 0] = "Moda";
+    ProductCategory[ProductCategory["Eletronicos"] = 1] = "Eletronicos";
+    ProductCategory[ProductCategory["Alimentos"] = 2] = "Alimentos";
+    ProductCategory[ProductCategory["Bebidas"] = 3] = "Bebidas";
+    ProductCategory[ProductCategory["CasaDecoracao"] = 4] = "CasaDecoracao";
+    ProductCategory[ProductCategory["SaudeBeleza"] = 5] = "SaudeBeleza";
+    ProductCategory[ProductCategory["EsporteLazer"] = 6] = "EsporteLazer";
+    ProductCategory[ProductCategory["Livros"] = 7] = "Livros";
+    ProductCategory[ProductCategory["Papelaria"] = 8] = "Papelaria";
+    ProductCategory[ProductCategory["Brinquedos"] = 9] = "Brinquedos";
+    ProductCategory[ProductCategory["Jogos"] = 10] = "Jogos";
+    ProductCategory[ProductCategory["Petshop"] = 11] = "Petshop";
+    ProductCategory[ProductCategory["Automotivos"] = 12] = "Automotivos";
+})(ProductCategory || (exports.ProductCategory = ProductCategory = {}));
